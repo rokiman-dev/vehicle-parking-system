4c98d9428e706c18cee73c0db46c61d66fcc3d26
Roki <rokiman1801@gmail.com>
2025-03-06 17:38:03 +0700

    ADD basic program structure with initialization message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ParkingSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Parking System Initialized");
         }
     }
 }
