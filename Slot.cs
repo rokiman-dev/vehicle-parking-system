0f826e9b936f3f86704483bd26e9c131534b6011
Roki <rokiman1801@gmail.com>
2025-03-06 17:50:01 +0700

    ADD Vehicle and Slot classes

---
+++ Slot.cs
@@ -0,0 +1,5 @@
+public class Slot
+{
+    public int SlotNumber { get; set; }
+    public Vehicle ParkedVehicle { get; set; }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,15 @@
+public enum VehicleType { Car, Motorcycle }
+
+public class Vehicle
+{
+    public string LicensePlate { get; set; }
+    public VehicleType Type { get; set; }
+    public string Color { get; set; }
+
+    public Vehicle(string lisencePlate, VehicleType type, string color)
+    {
+        LicensePlate = LicensePlate;
+        Type = type;
+        Color = color;
+    }
+}
